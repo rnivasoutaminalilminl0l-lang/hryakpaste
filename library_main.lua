a2ec3992b685a0f286dfbe902d50922c6dd502a0
rnivasoutaminalilminl0l-lang <rnivasoutaminalilminl0l@gmail.com>
2026-02-25 16:48:49 +0300

    Update library_main.lua

--- library_main.lua
+++ library_main.lua
@@ -1264,7 +1264,11 @@ do
                     local Key;
 
                     if Input.UserInputType == Enum.UserInputType.Keyboard then
-                        Key = Input.KeyCode.Name;
+                        if Input.KeyCode == Enum.KeyCode.Escape then
+                            Key = 'None';
+                        else
+                            Key = Input.KeyCode.Name;
+                        end
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton1 then
                         Key = 'MB1';
                     elseif Input.UserInputType == Enum.UserInputType.MouseButton2 then
